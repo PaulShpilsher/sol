8bf87b3f80733b72239e2fd1bf977a04028bad4b
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-11-24 14:57:52 +0300

    should have 0 balance by default

--- 01_testing_contract/test/PaymentsTest.js
+++ 01_testing_contract/test/PaymentsTest.js
@@ -3,7 +3,7 @@
 //   loadFixture,
 // } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 // const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-// const { expect } = require("chai");
+const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Payments", () => {
@@ -13,16 +13,26 @@ describe("Payments", () => {
 
   beforeEach(async () => {
     [ownerAccount, otherAccount] = await ethers.getSigners(); // get a couple of accounts
-    console.log(`Owner: ${ownerAccount.address}, otherAccount: ${otherAccount.address}`);
+    //console.log(`Owner: ${ownerAccount.address}, otherAccount: ${otherAccount.address}`);
     const Payments = await ethers.getContractFactory("Payments", ownerAccount); // create contract
-    payments = await Payments.deploy();  // deploying contract to BC
+    payments = await Payments.deploy();  // deploying contract to BC    
     await payments.waitForDeployment();  // wait unitl contract deployed 
-    console.log(`Contract deployed. Address: ${await payments.getAddress()}`);
+    // console.log(`Contract deployed. Address: ${await payments.getAddress()}`);
   });
 
 
-  it("should be deployed", async () => {console.log("success")});
+  it("should be deployed", async () => {
+    const paymentsAddress = await payments.getAddress();
+    expect(paymentsAddress).to.be.properAddress;
+  });
 
+  it("should have 0 balance by default", async () => {
+    const balance = await payments.currentBalance();
+    // console.log(balance); 
+    expect(balance).to.eq(0n);
+  });
+
+  
   
   
 });
