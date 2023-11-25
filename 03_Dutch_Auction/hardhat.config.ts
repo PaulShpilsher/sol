21fcbb8c19db89b808a68b36aba714b2a90fd46e
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-11-25 18:48:29 +0300

    move to ts

--- 03_Dutch_Auction/hardhat.config.ts
+++ 03_Dutch_Auction/hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "solidity-coverage";
+// This adds support for typescript paths mappings
+import "tsconfig-paths/register";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
--- 03_Dutch_Auction/test/AuctionEngine.ts
+++ 03_Dutch_Auction/test/AuctionEngine.ts
@@ -1,36 +1,45 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { AuctionEngine } from "../typechain-types";
 
-const log = (...args) => console.log(...args);
+const log = (...args: any[]) => console.log(...args);
+
+const getBlockTimestamp = async (blockNumber: number): Promise<number> => {
+  const block = await ethers.provider.getBlock(blockNumber);
+  return block!.timestamp;
+};
 
 describe("AuctionEngine", () => {
-
-  let ownerAccount, sellerAccount, buyerAccount;
-  let engine;
+  let ownerAccount: any;
+  let sellerAccount: any;
+  let buyerAccount: any;
+  let engine: AuctionEngine;
 
   beforeEach(async () => {
     [ownerAccount, sellerAccount, buyerAccount] = await ethers.getSigners(); // get a couple of accounts
-    const smartContract = await ethers.getContractFactory("AuctionEngine", ownerAccount); // create contract
-    engine = await smartContract.deploy();  // deploying contract to BC    
-    await engine.waitForDeployment();  // wait unitl contract deployed 
+    const smartContract = await ethers.getContractFactory(
+      "AuctionEngine",
+      ownerAccount
+    ); // create contract
+    engine = await smartContract.deploy(); // deploying contract to BC
+    await engine.waitForDeployment(); // wait unitl contract deployed
   });
 
-  
-  it("sets owner", async () => {    
+  it("sets owner", async () => {
     const engineOwnerAddress = await engine.owner();
     expect(engineOwnerAddress).to.eq(ownerAccount.address);
   });
 
-
   describe("createAuction", async () => {
-
     it("creates auction correctly", async () => {
       const item = "an awesome item";
       const startingPrice = ethers.parseEther("0.0001");
       // log("Starting price: ", startingPrice);
       const discountRate = 3;
       const duration = 60;
-      const tx = await engine.connect(sellerAccount).createAuction(item, startingPrice, discountRate, duration);
+      const tx = await engine
+        .connect(sellerAccount)
+        .createAuction(item, startingPrice, discountRate, duration);
 
       const auction = await engine.auctions(0);
       // log(auction);
@@ -43,11 +52,9 @@ describe("AuctionEngine", () => {
       expect(auction.discountRate).to.eq(discountRate);
       expect(auction.stopped).to.eq(false);
 
-      const {timestamp} = await ethers.provider.getBlock(tx.blockNumber);
+      const timestamp = await getBlockTimestamp(tx.blockNumber!);
       expect(auction.startAt).to.eq(timestamp);
       expect(auction.endsAt).to.eq(duration + timestamp);
     });
-
   });
-
 });
