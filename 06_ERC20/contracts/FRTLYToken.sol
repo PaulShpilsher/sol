172ac3a3061c49baff0a25d0ba8d591a53510150
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-11-26 17:04:06 +0300

    remove empty string

--- 06_ERC20/contracts/FRTLYToken.sol
+++ 06_ERC20/contracts/FRTLYToken.sol
@@ -6,7 +6,6 @@ import "./Erc20.sol";
 contract FRTLYToken is ERC20 {
 
     constructor(address _principal, uint _initialSupply) ERC20("FRTLY token", "FRTLY", _initialSupply, _principal) {
-
     }
 
 }
