fd68db1579d468cafb868366e7b3cdb4acaea43c
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-11-26 14:06:35 +0300

    inArray

--- 05_interfaces/contracts/ExtDemo.sol
+++ 05_interfaces/contracts/ExtDemo.sol
@@ -6,8 +6,13 @@ import "./Ext.sol";
 contract ExtDemo {
 
     using StrExt for string;
+    using ArrayExt for uint[];
 
     function areStringsEqual(string memory _x, string memory _y) public pure returns(bool) {
         return _x.eq(_y);
+    }
+
+    function inArray(uint[] memory _arr, uint _value) public pure returns(bool) {
+        return _arr.contains(_value);
     }
 }
