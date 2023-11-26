c5edd2ef78e0e9c1a945581edf8107f40712ec1c
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-11-26 17:03:22 +0300

    create FRTLY token

---
+++ 06_ERC20/contracts/FRTLYToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "./Erc20.sol";
+
+contract FRTLYToken is ERC20 {
+
+    constructor(address _principal, uint _initialSupply) ERC20("FRTLY token", "FRTLY", _initialSupply, _principal) {
+
+    }
+
+}
