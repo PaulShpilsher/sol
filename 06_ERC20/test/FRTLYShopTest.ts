14919aabf8da96e9bb26c45d57cd868214b899ca
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-12-02 13:37:10 +0300

    add test allows to buy

--- 06_ERC20/test/FRTLYShopTest.ts
+++ 06_ERC20/test/FRTLYShopTest.ts
@@ -1,6 +1,8 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { FRTLYShop } from "../typechain-types";
+import tokenJson from "../artifacts/contracts/FRTLYToken.sol/FRTLYToken.json";
+import { Contract } from "ethers";
 
 const log = (...args: any[]) => console.log(...args);
 
@@ -9,6 +11,7 @@ describe("FRTLYShop", () => {
   let buyer: any;
   let shop: FRTLYShop;
   let shopAddress: string;
+  let erc20: Contract;
 
   beforeEach(async () => {
     [owner, buyer] = await ethers.getSigners();
@@ -19,10 +22,35 @@ describe("FRTLYShop", () => {
 
     // log(shop);    
     shopAddress = await shop.getAddress();
-  });
+    erc20 = new ethers.Contract(await shop.token(), tokenJson.abi, owner); 
+    });
 
   it("should have owner and token", async () => {
     expect(await shop.owner()).to.equal(owner.address);
     expect(await shop.token()).to.be.properAddress;
   });
+
+
+  it("allows to buy", async () => {
+    const tokenAmount = 3;
+    const txData = {
+      value: tokenAmount,
+      to: shopAddress
+    };
+
+    const tx = await buyer.sendTransaction(txData);
+    await tx.wait();
+
+    //  check that the buyer got tokens
+    const buyerTokens = await erc20.balanceOf(buyer.address);
+    expect(buyerTokens).to.eq(tokenAmount)
+
+    // check that shop token amount decreased
+    await expect(() => tx).to.changeEtherBalance(shop, tokenAmount);
+
+    // check that we got Bought event
+    await expect(tx).to.emit(shop, "Bought").withArgs(tokenAmount, buyer.address);
+  });
+
+
 });
