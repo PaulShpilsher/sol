4734c2de16d9a8c37c8f9c18b6c759f72580bfc8
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-12-03 13:03:06 +0300

    add deploy for FE

--- 03_Dutch_Auction/scripts/deploy.ts
+++ 03_Dutch_Auction/scripts/deploy.ts
@@ -1,5 +1,9 @@
-import hte, { network, ethers } from "hardhat";
-import type { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/src/signers";
+import hte, { network, ethers, artifacts } from "hardhat";
+// import type { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/src/signers";
+import fs from "fs";
+import fsp from "fs/promises";
+import path from "path";
+import { BaseContract } from "ethers";
 
 async function main() {
   if (network.name === "hardhat") {
@@ -21,12 +25,61 @@ async function main() {
   const engine = await auctionEngineFactory.deploy(); // deploying contract to BC
   await engine.waitForDeployment(); // wait unitl contract deployed
 
-  console.log("Deployed at ", (await engine.getAddress());
+  console.log("Deployed at ", await engine.getAddress());
+
+  await saveFrontendFiles({
+    DutchAuctionEngine: engine,
+  });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function saveFrontendFiles(
+  contracts: Record<string, BaseContract>
+): Promise<void> {
+  const contractsDir = path.join(__dirname, "..", "front/contracts");
+
+  let exists;
+  try {
+    await fsp.access(contractsDir);
+    exists = true;
+  } catch (e) {
+    exists = false;
+  }
+
+  if (!exists) {
+    await fsp.mkdir(contractsDir);
+  }
+
+  const deploymentPromises = Object.entries(contracts).map(
+    async ([name, contract]) => {
+      if (contract) {
+        const contractAddress = await contract.getAddress();
+        const data = {
+          [name]: contractAddress,
+        };
+        const contractFile = path.join(
+          contractsDir,
+          `${name}-contract-address.json`
+        );
+        console.log("Writing: ", contractFile);
+        await fsp.writeFile(contractFile, JSON.stringify(data, undefined, 2));
+      }
+
+      const contractArtifact = await artifacts.readArtifact(name);
+      const contractArtifactFile = path.join(contractsDir, `${name}.json`);
+      console.log("Writing: ", contractArtifactFile);
+      await fsp.writeFile(
+        contractArtifactFile,
+        JSON.stringify(contractArtifact, undefined, 2)
+      );
+    }
+  );
+
+  await Promise.all(deploymentPromises);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
