073fa5d4d8ba3f56433ac547f4b9055f14993565
Paul Shpilsher <paul.shpilsher@gmail.com>
2023-12-25 12:29:25 +0300

    refactor

--- honeypot/contracts/Logger.sol
+++ honeypot/contracts/Logger.sol
@@ -3,12 +3,7 @@ pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
-
-interface ILogger {
-    event Log(address caller, uint amount, uint actionCode);
-
-    function log(address _caller, uint _amount, uint _actionCode) external;
-}
+import "./ILogger.sol";
 
 contract Logger is ILogger {
     function log(
